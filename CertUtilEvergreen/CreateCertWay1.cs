8a28d1a7e24f7f6c2c3a1aa47ba58eceb45fb68b
thuanpv1 <thuanpv@egt.vn>
2021-12-03 16:36:43 +0700

    add .net framework project to create .dll

---
+++ CertUtilEvergreen/CreateCertWay1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Text;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Prng;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Utilities;
+using Org.BouncyCastle.X509;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+namespace CertUtilEvergreen
+{
+    public class CreateCertWay1
+    {
+
+        public static X509Certificate2 GenerateSelfSignedCertificate(string subjectName, string issuerName, AsymmetricKeyParameter issuerPrivKey, int yearOfValid)
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            // Key Usage for this cert via cert generator
+            var keyUsage1 = new KeyUsage(KeyUsage.DigitalSignature | KeyUsage.KeyEncipherment);
+            certificateGenerator.AddExtension(X509Extensions.KeyUsage.Id, false, keyUsage1);
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            const string signatureAlgorithm = "SHA256WithRSA";
+            certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name(subjectName);
+            X509Name issuerDN = new X509Name(issuerName);
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(yearOfValid);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            var keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            var keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            // Generating the Certificate
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+
+            // selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(issuerPrivKey, random);
+            
+
+            // correcponding private key
+            PrivateKeyInfo info = PrivateKeyInfoFactory.CreatePrivateKeyInfo(subjectKeyPair.Private);
+
+
+            // merge into X509Certificate2
+            X509Certificate2 x509 = new X509Certificate2(certificate.GetEncoded());
+            bool isThisCertHasPrivateKey = x509.HasPrivateKey;
+
+            Asn1Sequence seq = (Asn1Sequence)Asn1Object.FromByteArray(info.PrivateKeyData.GetOctets());
+            if (seq.Count != 9)
+            {
+                //throw new PemException("malformed sequence in RSA private key");
+            }
+
+            RsaPrivateKeyStructure rsa = RsaPrivateKeyStructure.GetInstance(seq);
+            RsaPrivateCrtKeyParameters rsaparams = new RsaPrivateCrtKeyParameters(
+                rsa.Modulus, rsa.PublicExponent, rsa.PrivateExponent, rsa.Prime1, rsa.Prime2, rsa.Exponent1, rsa.Exponent2, rsa.Coefficient);
+
+            //x509.PrivateKey = ToDotNetKey(rsaparams); //x509.PrivateKey = DotNetUtilities.ToRSA(rsaparams);
+
+            X509Certificate2 finalX509 = RSACertificateExtensions.CopyWithPrivateKey(x509, (RSA)ToDotNetKey(rsaparams));
+            isThisCertHasPrivateKey = finalX509.HasPrivateKey;
+            return finalX509;
+
+        }
+
+        public static AsymmetricAlgorithm ToDotNetKey(RsaPrivateCrtKeyParameters privateKey)
+        {
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = Guid.NewGuid().ToString(),
+                KeyNumber = (int)KeyNumber.Exchange,
+                Flags = CspProviderFlags.UseMachineKeyStore
+            };
+
+            var rsaProvider = new RSACryptoServiceProvider(cspParams);
+            var parameters = new RSAParameters
+            {
+                Modulus = privateKey.Modulus.ToByteArrayUnsigned(),
+                P = privateKey.P.ToByteArrayUnsigned(),
+                Q = privateKey.Q.ToByteArrayUnsigned(),
+                DP = privateKey.DP.ToByteArrayUnsigned(),
+                DQ = privateKey.DQ.ToByteArrayUnsigned(),
+                InverseQ = privateKey.QInv.ToByteArrayUnsigned(),
+                D = privateKey.Exponent.ToByteArrayUnsigned(),
+                Exponent = privateKey.PublicExponent.ToByteArrayUnsigned()
+            };
+
+            rsaProvider.ImportParameters(parameters);
+            return rsaProvider;
+        }
+
+        public static X509Certificate2 GenerateCACertificate(string subjectName, ref AsymmetricKeyParameter CaPrivateKey, int yearOfValid, string privateKeyPemFilePath = "CA_PrivateKey_For_CreateOtherClientPFX.pem")
+        {
+            const int keyStrength = 2048;
+
+            // Generating Random Numbers
+            CryptoApiRandomGenerator randomGenerator = new CryptoApiRandomGenerator();
+            SecureRandom random = new SecureRandom(randomGenerator);
+
+            // The Certificate Generator
+            X509V3CertificateGenerator certificateGenerator = new X509V3CertificateGenerator();
+
+            // Serial Number
+            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(Int64.MaxValue), random);
+            certificateGenerator.SetSerialNumber(serialNumber);
+
+            // Signature Algorithm
+            const string signatureAlgorithm = "SHA256WithRSA";
+            certificateGenerator.SetSignatureAlgorithm(signatureAlgorithm);
+
+            // Issuer and Subject Name
+            X509Name subjectDN = new X509Name(subjectName);
+            X509Name issuerDN = subjectDN;
+            certificateGenerator.SetIssuerDN(issuerDN);
+            certificateGenerator.SetSubjectDN(subjectDN);
+
+            // Valid For
+            DateTime notBefore = DateTime.UtcNow.Date;
+            DateTime notAfter = notBefore.AddYears(yearOfValid);
+
+            certificateGenerator.SetNotBefore(notBefore);
+            certificateGenerator.SetNotAfter(notAfter);
+
+            // Subject Public Key
+            AsymmetricCipherKeyPair subjectKeyPair;
+            KeyGenerationParameters keyGenerationParameters = new KeyGenerationParameters(random, keyStrength);
+            RsaKeyPairGenerator keyPairGenerator = new RsaKeyPairGenerator();
+            keyPairGenerator.Init(keyGenerationParameters);
+            subjectKeyPair = keyPairGenerator.GenerateKeyPair();
+
+            certificateGenerator.SetPublicKey(subjectKeyPair.Public);
+
+            // Generating the Certificate
+            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+
+            // selfsign certificate
+            Org.BouncyCastle.X509.X509Certificate certificate = certificateGenerator.Generate(issuerKeyPair.Private, random);
+            X509Certificate2 x509 = new X509Certificate2(certificate.GetEncoded());
+            bool test = x509.HasPrivateKey;
+            CaPrivateKey = issuerKeyPair.Private;
+
+            // thuan added here
+            PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(CaPrivateKey);
+            byte[] serializedPrivateBytes = privateKeyInfo.ToAsn1Object().GetDerEncoded();
+            // Convert to Base64 ..
+            string serializedPrivateString = Convert.ToBase64String(serializedPrivateBytes);
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("-----BEGIN PRIVATE KEY-----");
+            builder.AppendLine(Convert.ToBase64String(serializedPrivateBytes, Base64FormattingOptions.InsertLineBreaks));
+            builder.AppendLine("-----END PRIVATE KEY-----");
+            Console.WriteLine(builder.ToString());
+            File.WriteAllText(privateKeyPemFilePath, builder.ToString());
+
+            return x509;
+            //return issuerKeyPair.Private;
+
+        }
+
+        public static bool addCertToStore(System.Security.Cryptography.X509Certificates.X509Certificate2 cert, System.Security.Cryptography.X509Certificates.StoreName st, System.Security.Cryptography.X509Certificates.StoreLocation sl)
+        {
+            bool bRet = false;
+
+            try
+            {
+                X509Store store = new X509Store(st, sl);
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(cert);
+
+                store.Close();
+            }
+            catch
+            {
+
+            }
+
+            return bRet;
+        }
+
+    }
+}
---
+++ CertUtilEvergreen/Features.cs
@@ -0,0 +1,64 @@
+﻿using Org.BouncyCastle.Crypto;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertUtilEvergreen
+{
+    public class Features
+    {
+        public static byte[] CreatePfxWay4FromPrivateKey(string privateKeyPath, string outputFileNameIfWritten = "clientCert.pfx", bool isWrittenOutputToFile = false, string soCMND = "soCMND", string hoVaTen = "Nguyen Van A", string diaChi = "Hanoi")
+        {
+            string privateKeyStr = File.ReadAllText(privateKeyPath);
+            AsymmetricKeyParameter privateKey = ReadPublicKey(privateKeyStr);
+            var clientCert = CreateCertWay1.GenerateSelfSignedCertificate("CN=" + hoVaTen + "|" + soCMND + ",L=" + diaChi + ",OU=iGreens,O=iGreens,C=VN", "CN=iGreens,L=Hanoi,OU=iGreens,O=iGreens,C=VN", privateKey, 20);
+            var p12 = clientCert.Export(X509ContentType.Pfx, "12345678");
+            if (isWrittenOutputToFile) ByteArrayToFile(outputFileNameIfWritten, p12);
+
+            return p12;
+        }
+        public static bool ByteArrayToFile(string fileName, byte[] byteArray)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    fs.Write(byteArray, 0, byteArray.Length);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception caught in process: {0}", ex);
+                return false;
+            }
+        }
+
+        public static Org.BouncyCastle.Crypto.AsymmetricKeyParameter ReadPublicKey(string publicKey)
+        {
+            Org.BouncyCastle.Crypto.AsymmetricKeyParameter keyParameter = null;
+
+            using (System.IO.TextReader reader = new System.IO.StringReader(publicKey))
+            {
+                Org.BouncyCastle.OpenSsl.PemReader pemReader =
+                    new Org.BouncyCastle.OpenSsl.PemReader(reader);
+
+                object obj = pemReader.ReadObject();
+
+                if ((obj is Org.BouncyCastle.Crypto.AsymmetricCipherKeyPair))
+                    throw new System.ArgumentException("The given publicKey is actually a private key.", "publicKey");
+
+                if (!(obj is Org.BouncyCastle.Crypto.AsymmetricKeyParameter))
+                    throw new System.ArgumentException("The given publicKey is not a valid assymetric key.", "publicKey");
+
+                keyParameter = (Org.BouncyCastle.Crypto.AsymmetricKeyParameter)obj;
+            }
+
+            return keyParameter;
+        } // End Function ReadPublicKey 
+    }
+}
